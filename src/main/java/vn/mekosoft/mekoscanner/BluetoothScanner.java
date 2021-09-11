82c9bff90bbc1f256fc388e3d21430c2908f8fa0
oaidinh98 <dinhoai23061998@gmail.com>
2021-09-11 17:46:41 +0700

    Complete commit

---
+++ src/main/java/vn/mekosoft/mekoscanner/BluetoothScanner.java
@@ -0,0 +1,10 @@
+package vn.mekosoft.mekoscanner;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class BluetoothScanner {
+    public static void helloWorld(Context mContext){
+        Toast.makeText(mContext, "Chạy ra ròi nòe", Toast.LENGTH_LONG).show();
+    }
+}
